b2133432cb9851cef8c1a35b5b7937cfeeb249bb
hector <h.rosales01@ufromail.cl>
2026-02-23 11:45:59 -0300

    feat: add class with data utils

---
+++ FastData.py
@@ -0,0 +1,388 @@
+from faker import Faker
+
+fake = Faker()
+
+class FastData:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def generate_name():
+        return fake.name()
+
+    @staticmethod
+    def generate_id():
+        return fake.uuid4()
+
+    @staticmethod
+    def generate_email():
+        return fake.email()
+
+    @staticmethod
+    def generate_password():
+        return fake.password()
+
+    @staticmethod
+    def generate_phone():
+        return fake.phone_number()
+
+    @staticmethod
+    def generate_address():
+        return fake.address()
+
+    @staticmethod
+    def generate_city():
+        return fake.city()
+
+    @staticmethod
+    def generate_state():
+        return fake.state()
+
+    @staticmethod
+    def generate_zipcode():
+        return fake.zipcode()
+
+    @staticmethod
+    def generate_country():
+        return fake.country()
+
+    @staticmethod
+    def generate_company():
+        return fake.company()
+
+    @staticmethod
+    def generate_job():
+        return fake.job()
+
+    @staticmethod
+    def generate_text():
+        return fake.text()
+
+    @staticmethod
+    def generate_sentence():
+        return fake.sentence()
+
+    @staticmethod
+    def generate_paragraph():
+        return fake.paragraph()
+
+    @staticmethod
+    def generate_date():
+        return fake.date()
+
+    @staticmethod
+    def generate_datetime():
+        return fake.date_time()
+
+    @staticmethod
+    def generate_time():
+        return fake.time()
+
+    @staticmethod
+    def generate_url():
+        return fake.url()
+
+    @staticmethod
+    def generate_email():
+        return fake.email()
+
+    @staticmethod
+    def generate_password():
+        return fake.password()
+
+    @staticmethod
+    def generate_phone():
+        return fake.phone_number()
+
+    @staticmethod
+    def generate_address():
+        return fake.address()
+
+    @staticmethod
+    def generate_city():
+        return fake.city()
+
+    @staticmethod
+    def generate_state():
+        return fake.state()
+
+    @staticmethod
+    def generate_zipcode():
+        return fake.zipcode()
+
+    @staticmethod
+    def generate_country():
+        return fake.country()
+
+    @staticmethod
+    def generate_company():
+        return fake.company()
+
+    @staticmethod
+    def generate_job():
+        return fake.job()
+
+    @staticmethod
+    def generate_text():
+        return fake.text()
+
+    @staticmethod
+    def generate_sentence():
+        return fake.sentence()
+
+    @staticmethod
+    def generate_paragraph():
+        return fake.paragraph()
+
+    @staticmethod
+    def generate_date():
+        return fake.date()
+
+    @staticmethod
+    def generate_datetime():
+        return fake.date_time()
+
+    @staticmethod
+    def generate_time():
+        return fake.time()
+
+    @staticmethod
+    def generate_url():
+        return fake.url()
+
+    @staticmethod
+    def generate_email():
+        return fake.email()
+
+    @staticmethod
+    def generate_password():
+        return fake.password()
+
+    @staticmethod
+    def generate_phone():
+        return fake.phone_number()
+
+    @staticmethod
+    def generate_address():
+        return fake.address()
+
+    @staticmethod
+    def generate_city():
+        return fake.city()
+
+    @staticmethod
+    def generate_state():
+        return fake.state()
+
+    @staticmethod
+    def generate_zipcode():
+        return fake.zipcode()
+
+    @staticmethod
+    def generate_country():
+        return fake.country()
+
+    @staticmethod
+    def generate_company():
+        return fake.company()
+
+    @staticmethod
+    def generate_job():
+        return fake.job()
+
+    @staticmethod
+    def generate_text():
+        return fake.text()
+
+    @staticmethod
+    def generate_sentence():
+        return fake.sentence()
+
+    @staticmethod
+    def generate_paragraph():
+        return fake.paragraph()
+
+    @staticmethod
+    def generate_date():
+        return fake.date()
+
+    @staticmethod
+    def generate_datetime():
+        return fake.date_time()
+
+    @staticmethod
+    def generate_time():
+        return fake.time()
+
+    @staticmethod
+    def generate_url():
+        return fake.url()
+
+    @staticmethod
+    def generate_email():
+        return fake.email()
+
+    @staticmethod
+    def generate_password():
+        return fake.password()
+
+    @staticmethod
+    def generate_phone():
+        return fake.phone_number()
+
+    @staticmethod
+    def generate_address():
+        return fake.address()
+
+    @staticmethod
+    def generate_city():
+        return fake.city()
+
+    @staticmethod
+    def generate_state():
+        return fake.state()
+
+    @staticmethod
+    def generate_zipcode():
+        return fake.zipcode()
+
+    @staticmethod
+    def generate_country():
+        return fake.country()
+
+    @staticmethod
+    def generate_company():
+        return fake.company()
+
+    @staticmethod
+    def generate_job():
+        return fake.job()
+
+    @staticmethod
+    def generate_text():
+        return fake.text()
+
+    @staticmethod
+    def generate_sentence():
+        return fake.sentence()
+
+    @staticmethod
+    def generate_paragraph():
+        return fake.paragraph()
+
+    @staticmethod
+    def generate_date():
+        return fake.date()
+
+    @staticmethod
+    def generate_datetime():
+        return fake.date_time()
+
+    @staticmethod
+    def generate_time():
+        return fake.time()
+
+    @staticmethod
+    def generate_url():
+        return fake.url()
+
+    @staticmethod
+    def generate_email():
+        return fake.email()
+
+    @staticmethod
+    def generate_password():
+        return fake.password()
+
+    @staticmethod
+    def generate_phone():
+        return fake.phone_number()
+
+    @staticmethod
+    def generate_address():
+        return fake.address()
+
+    @staticmethod
+    def generate_city():
+        return fake.city()
+
+    @staticmethod
+    def generate_state():
+        return fake.state()
+
+    @staticmethod
+    def generate_zipcode():
+        return fake.zipcode()
+
+    @staticmethod
+    def generate_country():
+        return fake.country()
+
+    @staticmethod
+    def generate_company():
+        return fake.company()
+
+    @staticmethod
+    def generate_job():
+        return fake.job()
+
+    @staticmethod
+    def generate_text():
+        return fake.text()
+
+    @staticmethod
+    def generate_sentence():
+        return fake.sentence()
+
+    @staticmethod
+    def generate_paragraph():
+        return fake.paragraph()
+
+    @staticmethod
+    def generate_date():
+        return fake.date()
+
+    @staticmethod
+    def generate_datetime():
+        return fake.date_time()
+
+    @staticmethod
+    def generate_time():
+        return fake.time()
+
+    @staticmethod
+    def generate_url():
+        return fake.url()
+
+    @staticmethod
+    def generate_email():
+        return fake.email()
+
+    @staticmethod
+    def generate_password():
+        return fake.password()
+
+    @staticmethod
+    def generate_phone():
+        return fake.phone_number()
+
+    @staticmethod
+    def generate_address():
+        return fake.address()
+
+    @staticmethod
+    def generate_city():
+        return fake.city()
+
+    @staticmethod
+    def generate_state():
+        return fake.state()
+
+    @staticmethod
+    def generate_zipcode():
+        return fake.zipcode()
+
+    @staticmethod
+    def generate_country():
+        return fake.country()
+
