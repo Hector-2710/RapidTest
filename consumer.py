ece2337fb0facf0e0822b9f59e3352ef3caa6cea
hector <h.rosales01@ufromail.cl>
2026-02-24 14:12:50 -0300

    feat: add some test

--- consumer.py
+++ consumer.py
@@ -4,12 +4,18 @@ from FastData import FastData
 fastTest = FastTest("http://localhost:8000")
 
 print("test 1")
-fastTest.get("/", expected_status=200, response_data={"Hello": "World"})
+fastTest.get("/", expected_status=200, expected_body={"Hello": "World"})
 
 print("test 2")
-id = FastData.generate_id()
-fastTest.get(f"/post/{id}", expected_status=200, response_data={"id": id})
+fastTest.get("/", expected_status=200, expected_body={"Hello": "Wrong"})
 
 print("test 3")
-id = FastData.generate_id()
-fastTest.get("/post", expected_status=200, params={"id": id}, response_data={"id": id})
+fastTest.get("/", expected_status=404, expected_body={"Hello": "Wrong"})
+
+# print("test 2")
+# id = FastData.generate_id()
+# fastTest.get(f"/post/{id}", expected_status=200, response_data={"id": id})
+
+# print("test 3")
+# id = FastData.generate_id()
+# fastTest.get("/post", expected_status=200, params={"id": id}, response_data={"id": id})
