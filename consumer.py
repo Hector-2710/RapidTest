ce38fa6cd6e5844128f430639258dcd6c094e4b1
hector <h.rosales01@ufromail.cl>
2026-02-25 12:55:17 -0300

    feat: add test for post endpoint

--- consumer.py
+++ consumer.py
@@ -3,19 +3,17 @@ from FastData import FastData
 
 fastTest = FastTest("http://localhost:8000")
 
-print("test 1")
-fastTest.get("/", expected_status=200, expected_body={"Hello": "World"})
+# test 1
+# fastTest.get("/", expected_status=200, expected_body={"Hello": "World"})
 
-print("test 2")
-fastTest.get("/", expected_status=200, expected_body={"Hello": "Wrong"})
-
-print("test 3")
-fastTest.get("/", expected_status=404, expected_body={"Hello": "Wrong"})
-
-# print("test 2")
+# test 2
 # id = FastData.generate_id()
 # fastTest.get(f"/post/{id}", expected_status=200, response_data={"id": id})
 
-# print("test 3")
+# test 3
 # id = FastData.generate_id()
 # fastTest.get("/post", expected_status=200, params={"id": id}, response_data={"id": id})
+
+# test 4
+user = FastData.generate_auth_user()
+fastTest.post("/user", expected_status=201, data=user, expected_body=user)
