c851cb419c027f03ca0dfadd4377f782d60b145f
hector <h.rosales01@ufromail.cl>
2026-02-28 00:00:32 -0300

    feat: update num of the version

--- consumer.py
+++ consumer.py
@@ -1,26 +1,26 @@
-from rapidtest.RapidTest import FastTest
-from rapidtest.RapidData import FastData
+from rapidtest.RapidTest import Test
+from rapidtest.RapidData import Data
 
-fastTest = FastTest("http://localhost:8000")
+fastTest = Test(url="http://localhost:8000")
 
-# test 1
+# # test 1
 # fastTest.get("/", expected_status=200, expected_body={"Hello": "World"})
 
-# test 2
-# id = FastData.generate_id()
-# fastTest.get(f"/post/{id}", expected_status=200, response_data={"id": id})
+# # test 2
+# id = Data.generate_id()
+# fastTest.get(f"/post/{id}", expected_status=404, expected_body={"id": id})
 
 # test 3
-# id = FastData.generate_id()
-# fastTest.get("/post", expected_status=200, params={"id": id}, response_data={"id": id})
+id = Data.generate_id()
+fastTest.get("/post", expected_status=200, params={"id": id}, expected_body={"id": id})
 
-# test 4: POST
-user = FastData.generate_auth_user()
-fastTest.post("/user", expected_status=201, data=user, expected_body=user)
+# # test 4: POST
+# user = Data.generate_auth_user()
+# fastTest.post("/user", expected_status=201, data=user, expected_body=user)
 
-# test 5: PUT
-new_password = FastData.generate_password()
-fastTest.put(f"/user/{user['username']}", data={"password": new_password}, expected_status=200)
+# # test 5: PUT
+# new_password = Data.generate_password()
+# fastTest.put(f"/user/{user['username']}", data={"password": new_password}, expected_status=200)
 
-# test 6: DELETE
-fastTest.delete(f"/user/{user['username']}", expected_status=200)
+# # test 6: DELETE
+# fastTest.delete(f"/user/{user['username']}", expected_status=200)
