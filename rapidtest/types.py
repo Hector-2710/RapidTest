1bdfc58d95bc91b67b5cf21325e082de30bfd14b
hector <h.rosales01@ufromail.cl>
2026-03-07 11:50:39 -0300

    ✨ feat : add class for better status code management

--- rapidtest/types.py
+++ rapidtest/types.py
@@ -1,5 +1,6 @@
 from typing import Union, Any
 import requests
+import enum
 
 """
 Type definitions for the RapidTest library.
@@ -15,10 +16,24 @@ type QueryParams = dict[str, Any]
 type JsonData = Union[dict[str, Any], list, str, int, float, bool]
 type RawData = Union[str, bytes, dict[str, Any]]
 type HttpMethod = str
-type StatusCode = int
 type Endpoint = str
 type Response = Union[requests.Response, None]
 type Results = dict[str, Any]
 
-
+class StatusCode(enum.IntEnum):
+    CONTINUE = 100
+    OK = 200
+    CREATED = 201
+    ACCEPTED = 202
+    NO_CONTENT = 204
+    MOVED_PERMANENTLY = 301
+    TEMPORARY_REDIRECT = 307
+    PERMANENT_REDIRECT = 308
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    NOT_ACCEPTABLE = 406
+    REQUEST_TIMEOUT = 408
+    INTERNAL_SERVER_ERROR = 500
 
