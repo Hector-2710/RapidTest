6b858ab5c4b53ca0dba4c46fe5dd1115ab169a8c
hector <h.rosales01@ufromail.cl>
2026-03-07 11:54:29 -0300

    ✨ feat: add StatusCode import

--- rapidtest/__init__.py
+++ rapidtest/__init__.py
@@ -5,5 +5,6 @@ RapidTest: A library for simplifying REST API testing.
 from .RapidTest import Test as Test
 from .RapidData import data as data
 from .Performance import Performance as Performance
+from .types import StatusCode as StatusCode
 
 
