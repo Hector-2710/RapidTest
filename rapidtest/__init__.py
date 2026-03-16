d843cc6629f4ce409366f8c92556181337711f8c
hector <h.rosales01@ufromail.cl>
2026-03-16 14:55:29 -0300

    ✅ refactor: update import statement for StatusCode to improve module organization

--- rapidtest/__init__.py
+++ rapidtest/__init__.py
@@ -7,7 +7,7 @@ Includes traditional HTTP testing and ASGI-level testing for maximum performance
 from .Test import Test as Test
 from .data import data as data
 from .Performance import Performance as Performance
-from .Utils import StatusCode as StatusCode
+from .StatusCode import StatusCode as StatusCode
 
 
 
