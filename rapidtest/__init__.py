4acd9cb5e039884e3dc0514794632c66f884e794
hector <h.rosales01@ufromail.cl>
2026-04-08 18:36:08 -0400

    ✅ refactor: update __init__.py exports with HTTPTest and ASGITest

--- rapidtest/__init__.py
+++ rapidtest/__init__.py
@@ -4,8 +4,8 @@ RapidTest: A library for simplifying REST API testing.
 Includes traditional HTTP testing, ASGI-level testing and Performance testing.
 """
 
-from .test import Test as Test
+from .http_test import HTTPTest as HTTPTest
+from .asgi_test import ASGITest as ASGITest
 from .data import Data as Data
 from .performance import Performance as Performance
 from .status_code import StatusCode as StatusCode
-
